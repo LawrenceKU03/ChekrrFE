127b0aa7966af1b4dda5524f8d74bd4546ba36e6
Lawrence <lawuche29@gmail.com>
2026-03-03 15:43:05 +0100

    chore:made header mobile responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import Header from "./components/Header";
 
 function App() {
 	return (
-		<>
+		<div className="w-screen overflow-hidden">
 			<Header />
 			<Routes></Routes>
-		</>
+		</div>
 	);
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { SiWhatsapp } from "react-icons/si";
 
 interface NavLink {
 	title: string;
@@ -22,18 +23,48 @@ const index = () => {
 	]);
 
 	return (
-		<div className="bg-white flex flex-row items-center justify-between px-6 py-8">
-			<h1 className="font-bold font-['Montserrat'] text-4xl">Chekrr</h1>
-			<div className="flex flex-row items-center justify-between">
-				{NavLinks.map((link) => {
-					return (
-						<a href={link.href} className="font-['Poppins'] mx-[20px]">
-							{link.title}
-						</a>
-					);
-				})}
+		<div className="bg-white flex flex-col">
+			<div className="flex flex-row items-center justify-between md:px-24 md:py-8 py-6 px-4">
+				<h1 className="font-bold font-['Montserrat'] text-4xl bg-indigo-500 px-4 py-2 text-white">
+					Chekrr
+				</h1>
+				<div className="flex flex-row items-center justify-between md:block hidden">
+					{NavLinks.map((link) => {
+						return (
+							<a
+								href={link.href}
+								className="font-['Poppins'] mx-[20px] opacity-50 hover:opacity-100 transition font-semibold"
+							>
+								{link.title}
+							</a>
+						);
+					})}
+				</div>
+				<button
+					className="bg-green-500 text-white font-bold uppercase p-3 px-4 rounded-[24px] hidden md:flex flex-row items-center"
+					style={{ flexDirection: "row" }}
+				>
+					Get Started <SiWhatsapp className="ml-4" size={24} />
+				</button>
+				<button className="bg-green-500 text-white font-bold uppercase p-3 px-4 block md:hidden rounded-lg flex flex-row items-center">
+					<SiWhatsapp size={28} />
+				</button>
 			</div>
-			<button>Get Started</button>
+
+			<div className="md:hidden block">
+				<div className="flex flex-row items-center justify-between my-6 mx-2">
+					{NavLinks.map((link) => {
+						return (
+							<a
+								href={link.href}
+								className="font-['Poppins'] mx-[20px] opacity-50 hover:opacity-100 transition font-semibold"
+							>
+								{link.title}
+							</a>
+						);
+					})}
+				</div>
+			</div>
 		</div>
 	);
 };
